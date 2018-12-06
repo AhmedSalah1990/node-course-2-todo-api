5827448fe8581df997f78e584a5fa437372d5166
ahmed <dr.ahmedsalah1990@gmail.com>
2018-12-06 19:42:32 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -158,6 +158,17 @@ app.post("/users/login", (req, res) => {
     });
 });
 
+app.delete("/users/me/token", authonticate, (req, res) => {
+  req.user.removeToken(req.token).then(
+    () => {
+      res.status(200).send();
+    },
+    () => {
+      res.status(400).send();
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
